8b4d24f926702c80db1f68fbbcd3f1b9f82b0d12
marciozampiron <marciocastelobranco3@gmail.com>
2026-03-16 01:40:15 -0300

    test(oci): remove unused log_warn from smoke script

--- scripts/e2e/oci_smoke.sh
+++ scripts/e2e/oci_smoke.sh
@@ -21,10 +21,6 @@ log_pass() {
     printf '[\033[0;32mPASS\033[0m] %s\n' "$1"
 }
 
-log_warn() {
-    printf '[\033[0;33mWARN\033[0m] %s\n' "$1"
-}
-
 log_fail() {
     printf '[\033[0;31mFAIL\033[0m] %s\n' "$1"
     exit 1
